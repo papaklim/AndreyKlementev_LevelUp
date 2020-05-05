29c4a58c5382263cb011baf6d47ff45bf3945688
papaklim <papaklim@gmail.com>
2020-05-05 14:46:57 +0300

    del src from root

--- src/main/java/ru/levelup/andrey/klementev/qa/homework_1/calculator/Subtractor.java
+++
@@ -1,8 +0,0 @@
-package ru.levelup.andrey.klementev.qa.homework_1.calculator;
-
-public class Subtractor {
-
-    public int subtraction(int a, int b) {
-        return a - b;
-    }
-}
--- src/main/java/ru/levelup/andrey/klementev/qa/homework_1/calculator/Summator.java
+++
@@ -1,9 +0,0 @@
-package ru.levelup.andrey.klementev.qa.homework_1.calculator;
-
-public class Summator {
-
-    public double summation(double a, double b) {
-        return a + b;
-    }
-}
-
